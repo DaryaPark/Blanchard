ce19283ef13dc568587dbf4198e9c2f4ee3ce7ad
DaryaPark <89138103+DaryaPark@users.noreply.github.com>
2021-10-25 15:33:17 +0300

    adjusting indents

--- Blanchard/js/swiper.js
+++ Blanchard/js/swiper.js
@@ -17,6 +17,27 @@ onlyInViewport:true,
 pageUpDown:true,}
   },
  
+  // автопрокручивание
+  autoplay:{
+    // Пауза между прокруткой
+    delay:3000,
+    // закончить на последнем слайде
+    stopOnLastSlide:false,
+    // Отключить после ручного переключения
+    disableOnInteraction:false,
+},
+// скорость
+speed:800,
+
+// Эффект переключения слайдов.
+// смена  прозрачности.
+effect:'fade',
+
+// дополнение к fade
+fadeEffect:{
+    // Параллельная смена прозрачности
+    crossFade:true
+},
 });
 
 
