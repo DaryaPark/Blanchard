7b7f857487cf65f300a295cf3582ca9a560c0eea
DaryaPark <89138103+DaryaPark@users.noreply.github.com>
2022-01-17 16:13:18 +0300

    edit tooltip and map

--- Blanchard/js/mycode/tooltip-1.js
+++ Blanchard/js/mycode/tooltip-1.js
@@ -1,10 +1,39 @@
-  const button = document.querySelectorAll('#tbtn-1');
-  const tooltip = document.querySelectorAll('#tooltip-1');
+let tooltipElem;
 
-  // Pass the button, the tooltip, and some options, and Popper will do the
-  // magic positioning for you:
-  Popper.createPopper(button, tooltip, {
-    placement:'top',
-  });
+    document.onmouseover = function(event) {
+      let target = event.target;
 
-  
+      // если у нас есть подсказка...
+      let tooltipHtml = target.dataset.tooltip;
+      if (!tooltipHtml) return;
+
+      // ...создадим элемент для подсказки
+
+      tooltipElem = document.createElement('button');
+      tooltipElem.className = 'tooltip';
+      tooltipElem.innerHTML = tooltipHtml;
+      document.body.append(tooltipElem);
+
+      // спозиционируем его сверху от аннотируемого элемента (top-center)
+      let coords = target.getBoundingClientRect();
+
+      let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+      if (left < 0) left = 0; // не заезжать за левый край окна
+
+      let top = coords.top - tooltipElem.offsetHeight - 5;
+      if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+        top = coords.top + target.offsetHeight + 5;
+      }
+
+      tooltipElem.style.left = left + 'px';
+      tooltipElem.style.top = top + 'px';
+    };
+
+    document.onmouseout = function(e) {
+
+      if (tooltipElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+      }
+
+    };
