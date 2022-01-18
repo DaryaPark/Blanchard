42dc98bdfa579ada609a3517e6381fe36007fa13
DaryaPark <89138103+DaryaPark@users.noreply.github.com>
2022-01-18 15:17:08 +0300

    included files&Map scroll disabled

---
+++ Blanchard/js/mycode/mymap.js
@@ -0,0 +1,35 @@
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.75846806898367, 37.60108849999989],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 16,
+  });
+  myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+  myMap.controls.remove('searchControl'); // удаляем поиск
+  myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+  myMap.controls.remove('typeSelector'); // удаляем тип
+  myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  myMap.controls.remove('rulerControl'); // удаляем контрол правил
+  myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  myMap.behaviors.disable(['drag']); // отключаем прокрутку карты
+
+var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
+  iconLayout: 'default#image',
+  iconImageHref: 'img/icons/point-map.svg',
+  iconImageSize: [28, 40],
+  iconImagOffset: [-3, -42]
+});
+ // Размещение геообъекта на карте.
+myMap.geoObjects.add(myPlacemark);
+}
